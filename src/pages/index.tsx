304795e59a0bcf17dd270def0644d6fc4c5061ff
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-28 19:18:25 +0600

    building the header section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,18 +2,18 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import Header from "../../components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
+    <div>
       <Head>
-        <title>Abdul Kaiyum Fahim's Portfolio</title>
+        <title>Abdul Kaiyum Fahim</title>
       </Head>
-      <h1 className="text-blue-800">Lets build a awasome portfolio</h1>
-      <h2>This is seriously amazing from me</h2>
-      <h2>This is seriously outstanding from me</h2>
-    </main>
+      {/*Header */}
+      <Header></Header>
+    </div>
   );
 }
