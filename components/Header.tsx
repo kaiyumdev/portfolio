5b7b35b530237b07da7518d029c6761338ea5cd9
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-28 20:28:27 +0600

    Implementing React Social Icons Library

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>I am a Header</div>;
+  return (
+    <header>
+      <div className="flex flex-row items-center">
+        <SocialIcon
+          url="https://twitter.com/kaiyumdev"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com/kaiyumdev"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://twitter.com/kaiyumdev"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+      <div>
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
