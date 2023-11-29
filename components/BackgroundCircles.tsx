c35561a5046f39c3b531da93d3a311299ed56ecc
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-30 03:24:54 +0600

    start Building the About Section

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -16,8 +16,8 @@ const BackgroundCircles = (props: Props) => {
       className="relative  justify-center flex items-center"
     >
       <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
-      <div className="absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 " />
-      <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52 " />
+      <div className="absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52" />
+      <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52" />
       <div className="rounded-full border border-[#F7AB0A] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse " />
       <div className="rounded-full border border-[#333333] h-[800px] w-[800px] absolute mt-52 " />
     </motion.div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
+import profile from "../src/assets/images/profile.jpg";
+import Link from "next/link";
 
 type Props = {};
 
@@ -17,10 +20,36 @@ const Hero = (props: Props) => {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles></BackgroundCircles>
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A"></Cursor>
-      </h1>
+      <Image
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        // width={500}
+        // height={500}
+        alt=""
+        src={profile}
+      ></Image>
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px] ">
+          Software Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A"></Cursor>
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
