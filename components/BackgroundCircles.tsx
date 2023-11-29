3226bc50823918b02c13464037fc8dd5c1900700
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-11-29 11:59:56 +0600

    Adding Background Circles with Framer Motion

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const BackgroundCircles = (props: Props) => {
   return (
     <div className="relative flex items-center justify-center">
-      <div className="absolute border border-white rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
+      <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
       <div className="absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 " />
       <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52 " />
       <div className="rounded-full border border-[#F7AB0A] opacity-20 h-[650px] w-[650px] absolute mt-52 animate-pulse " />
