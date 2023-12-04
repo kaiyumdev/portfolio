8d1f9a70fcfafda8ce017ba341fb7265ab01d7b8
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-12-05 01:09:49 +0600

    projects section successfully completed

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,19 +1,32 @@
 /* eslint-disable react/jsx-key */
 import React from "react";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 const Projects = (props: Props) => {
   const projects = [1, 2, 3, 4, 5];
   return (
-    <div className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
       <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
         {projects.map((project, i) => (
           <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-            <img
+            <motion.img
+              initial={{
+                y: -300,
+                opacity: 0,
+              }}
+              transition={{ duration: 1.2 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
               src="https://th.bing.com/th/id/OIP.I5XOh9o_kbTNsXnsIc53aAHaHa?w=172&h=180&c=7&r=0&o=5&pid=1.7"
               alt=""
             />
@@ -24,12 +37,17 @@ const Projects = (props: Props) => {
                   Case study {1 + i} of {projects.length}: UPS clone
                 </span>
               </h4>
+              <p className="text-lg text-center md:text-left">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos
+                iste quis eveniet expedita saepe voluptatem dolorem minima at
+                rerum amet.
+              </p>
             </div>
           </div>
         ))}
       </div>
       <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12 "></div>
-    </div>
+    </motion.div>
   );
 };
 
